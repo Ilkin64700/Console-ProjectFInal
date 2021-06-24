29a35b568004b1784fcbfa635682ef446e5dab50
ilkin64700 <ilkinsz@code.edu.az>
2021-06-24 20:26:43 +0400

    Interfaces and Methods were created. Tasks(conditions) are assigned to Methods

---
+++ ConsoleProject/ConsoleProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleProject.Models;
+using System;
+
+namespace ConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+<<<<<<< HEAD
+            Employee employee = new Employee("ilkin","zamanli","muhasib",500,"maliyye");
+            Console.WriteLine(employee);
+            Department department = new Department();
+            Console.WriteLine(department.CalcSalarayAverage());
+        }
+=======
+
+        }
+        
+>>>>>>> 929f971 (Interfaces and Methods were created, Conditions are assigned to methods)
+    }
+}
