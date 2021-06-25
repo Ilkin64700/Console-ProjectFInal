3b79ce5969e59c8e1b1c94d472b3344f3d20e9e9
ilkin64700 <ilkinsz@code.edu.az>
2021-06-25 12:17:38 +0400

    Interfaces and Methods are created.Conditions are assigned in Methods

--- ConsoleProject1/Interfaces/IHumanResourceManager.cs
+++ ConsoleProject1/Interfaces/IHumanResourceManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleProject1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace ConsoleProject1.Interfaces
 {
     interface IHumanResourceManager
     {
+        List<Department> Departments { get; set; }
+        void AddDepartment(string name, int workerlimit, double salarylimit);
+        List<Department> GetDepartments();
+        void EditDepartment(string name, string newname);
+        void AddEmployee(string fullname, string position, double salary, string departmentname, string Id, List<Employee> Employees);
+        void RemoveEmployee(string Id, string departmmentname, Employee employee, List<Employee> Employees);
+        void EditEmployee(string Id, string fullname, double salary, string position, List<Employee> Employees);
     }
 }
